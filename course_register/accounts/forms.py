eb96a459c85ddfb75b2e62d443951fcbb2fb0053
keshavbadaya14 <keshav.badaya14@gmail.com>
2025-02-05 23:07:08 +0530

    change username to email in forms.py

--- course_register/accounts/forms.py
+++ course_register/accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Student
 from django.contrib.auth.hashers import make_password
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=100)
+    email = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
 
 class SignupForm(forms.ModelForm):
