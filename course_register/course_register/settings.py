10fd1b0d08d5be7a9562121e8bf91d2dc58d44c3
Sapparapu Abhijeet <153370234+sbndAbhijeet@users.noreply.github.com>
2025-10-21 18:30:48 +0530

    issue in allowed hosts

--- course_register/course_register/settings.py
+++ course_register/course_register/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
